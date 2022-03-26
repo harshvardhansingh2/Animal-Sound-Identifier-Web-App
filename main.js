7db42f363cd46ff8e15d9d79c6262f2080e22234
harshvardhansingh2 <97771241+harshvardhansingh2@users.noreply.github.com>
2022-03-25 19:24:34 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,48 @@
+var dog = 0;
+var cat = 0;
+var lion = 0;
+var cow = 0;
+
+function startClassification()
+{
+    navigator.mediaDevices.getUserMedia({ audio: true});
+    classifier = ml5.soundClassifier('https://teachablemachine.withgoogle.com/models/pk20USPQa/model.json', modelReady);
+}
+
+function modelReady(){
+    classifier.classify(gotResults);
+}
+
+function gotResults(error, results) {
+    if(error) {
+        console.error(error);
+    } else {
+        console.log(results);
+        random_number_r = Math.floor(Math.random() * 255) +1;
+        random_number_g = Math.floor(Math.random() * 255) +1;
+        random_number_b = Math.floor(Math.random() * 255) +1;
+
+        document.getElementById('result_label').innerHTML = 'I can hear - ' + results[0].label;
+        document.getElementById('result_count').innerHTML = 'Detected Dog - ' + dog + ' Detected Cat - ' + cat + ' Detected Lion - ' + lion + ' Detected Cow - ' + cow;
+        document.getElementById('result_label').style.color = "rgb(" + random_number_r + "," + random_number_g + "," + random_number_b + ")";
+        document.getElementById('result_count').style.color = "rgb(" + random_number_r + "," + random_number_g + "," + random_number_b + ")";
+
+        img = document.getElementById('animal_image');
+
+        if(results[0].label == "Barking") {
+            img.src = 'dog.png';
+            dog = dog + 1;
+        } else if (results[0].label == "Meowing") {
+            img.src = 'cat.jpg';
+            cat = cat + 1;
+        } else if (results[0].label == "Roar") {
+            img.src = 'lion.jpg';
+            lion = lion + 1;
+        } else if (results[0].label == "Mooing") {
+            img.src = 'cow.jpg';
+            cow = cow + 1;
+        } else {
+                img.src = 'ear.jpg';
+        }
+    }
+}
